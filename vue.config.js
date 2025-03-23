bbc83ed4b9d1ac0a13c3098ee95c0f23e006a975
Silici8m <vik.forc38@gmail.com>
2025-03-23 20:13:54 +0100

    v1 events liste

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,35 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/sylvart-website/" : "/"
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/sylvart-website/" : "/",
+  
+  // Fusionner avec la configuration de compression des images
+  chainWebpack(config) {
+    // Configuration de compression des images
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        mozjpeg: {
+          progressive: true,
+          quality: 70, // Qualité des images JPEG (vous pouvez ajuster)
+        },
+        optipng: {
+          enabled: true,
+          optimizationLevel: 7, // Niveau de compression PNG (0-7)
+        },
+        pngquant: {
+          quality: [0.6, 0.8], // Compression PNG
+          speed: 4, // Vitesse de compression (1-10)
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        webp: {
+          quality: 75, // Qualité des images WebP
+        },
+      });
+  }
+});
